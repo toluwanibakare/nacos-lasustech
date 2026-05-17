170f50e351174d01ac5ae2cc05ea8efc6cbe5def
TMB <mosesbakare48@gmail.com>
2026-05-17 01:10:58 +0100

    Change email transport configuration to use port 587

--- backend/services/email.service.js
+++ backend/services/email.service.js
@@ -5,8 +5,8 @@ dotenv.config();
 
 const transporter = nodemailer.createTransport({
   host: process.env.SMTP_HOST || 'smtp.gmail.com',
-  port: 465,
-  secure: true,
+  port: 587,
+  secure: false,
   auth: {
     user: process.env.SMTP_USER,
     pass: process.env.SMTP_PASS,
